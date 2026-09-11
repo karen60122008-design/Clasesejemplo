96799b9bb303d928db823ba4e3983cb5b213d29f
karen60122008-diseño <karen60122008@gmail.com>
2026-09-11 11:02:36 -0500

    octavo cambio

---
+++ arreglos.js
@@ -0,0 +1,14 @@
+let lista = []
+lista.push("a")
+console.log(lista)
+lista.push("b")
+console.log(lista)
+//agregar al inicio de un arreglo unshift
+lista.unshift("C")
+console.log(lista)
+//pop=eliminar elementos al final
+lista.pop()
+console.log(lista)
+//shift=elimina al inicio
+lista.shift()
+console.log(lista)
--- index.js
+++ index.js
@@ -1,2 +1,61 @@
-console.log("soyy karen")
-console.log("toby andres")
+class Animal {
+    nombre
+    raza
+    especie
+    edad
+
+    constructor(nombre, raza, especie, edad) {
+        this.nombre = nombre
+        this.raza = raza
+        this.especie = especie
+        this.edad = edad
+    }
+    registrarNombre(nuevonombre) {
+        this.nombre = nuevonombre
+    }
+    registrarRaza(nuevaRaza) {
+        this.raza = nuevaRaza
+    }
+    registrarEspecie(nuevaEspecie) {
+        this.especie = nuevaEspecie
+    }
+    registrarEdad(nuevaEdad) {
+        this.edad = nuevaEdad
+    }
+    mostrarInformacion() {
+        console.log(this.nombre + this.raza)
+    }
+}
+
+class Propietario {
+    nombrePropietario
+    Direccion
+    Telefono
+    constructor(nombre, direccion, telefono) {
+        this.nombrePropietario = nombre
+        this.Direccion = direccion
+        this.telefono = telefono
+        this.mascota = []
+    }
+    registrarNombre(nuevoNombre) {
+        this.nombrePropietario = nuevoNombre
+    }
+    registrarDireccion(nuevaDireccion) {
+        this.Direccion = nuevaDireccion
+    }
+    registrarTelefono(nuevaTelefono) {
+        this.telefono = nuevaTelefono
+    }
+    registrarMascota(nuevamascota) {
+        //pop,unshift,shift,push
+        this.mascota.push(nuevamascota)
+    }
+    mostrarInformacion(){
+        console.log("mascota: "+this.mascota[0].nombre + " Propietario: "+this.nombrePropietario)
+    }
+}
+let colmillo = new Animal("colmillo", "pitbull", "perro", "123")
+colmillo.mostrarInformacion()
+let karen = new Propietario("Karen", "123", "1111")
+karen.registrarMascota(colmillo)
+karen.mostrarInformacion()
